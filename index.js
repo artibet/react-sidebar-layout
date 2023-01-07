06edb1e039ca189bbe29241e193354cda99d6e59
Tsakiridis Sotiris <artibetgr@gmail.com>
2023-01-07 23:52:38 +0200

    Remove Test component

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ export { SidebarLayout } from './dist/esm'
 export { SidebarLayoutContext } from './dist/esm'
 export { defaultDarkTheme } from './dist/esm'
 export { defaultLightTheme } from './dist/esm'
-export { Test } from './dist/esm'
 
 
 
