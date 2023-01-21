2484145764384fb927531349153eda2a05106567
Tsakiridis Sotiris <artibetgr@gmail.com>
2023-01-21 22:03:22 +0200

    Remove theme files from dist export

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 export { SidebarLayout } from './dist/esm'
-export { defaultDarkTheme } from './dist/esm'
-export { defaultLightTheme } from './dist/esm'
 export { useSidebarLayoutContext } from './dist/esm'
 
 
